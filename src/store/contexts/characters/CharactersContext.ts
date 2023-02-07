55c009dedbd7960b66f6b9f1a5c96996ae81f314
AnaCatanaR <anacatana.ac@gmail.com>
2023-02-07 21:30:56 +0100

    Create CharacterContextProvider

--- src/store/contexts/characters/CharactersContext.ts
+++ src/store/contexts/characters/CharactersContext.ts
@@ -1,8 +1,10 @@
 import { createContext } from "react";
 import { CharacterStructure } from "../../../data/types";
+import { CharactersAction } from "../../actions/characters/types";
 
 interface CharactersContextStructure {
-  characters: CharacterStructure;
+  characters: CharacterStructure[];
+  dispatch: React.Dispatch<CharactersAction>;
 }
 
 const CharactersContext = createContext({} as CharactersContextStructure);
