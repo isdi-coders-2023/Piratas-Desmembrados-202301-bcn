6e676be7af2b554e30ceeca0c11b8e2e954c3f06
AnaCatanaR <anacatana.ac@gmail.com>
2023-02-10 20:14:16 +0100

    Add Burger Component to Header

---
+++ src/components/Burger/Burger.tsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import RightNav from "../RightNav/RightNav";
+import { BurgerStyled } from "./BurgerStyled";
+
+export const Burger = (): JSX.Element => {
+  const [open, setOpen] = useState(false);
+
+  const changeBurgerState = () => {
+    setOpen(!open);
+  };
+
+  return (
+    <>
+      <BurgerStyled onClick={changeBurgerState} open={open}>
+        <div />
+        <div />
+        <div />
+      </BurgerStyled>
+      <RightNav isOpen={open} />
+    </>
+  );
+};
