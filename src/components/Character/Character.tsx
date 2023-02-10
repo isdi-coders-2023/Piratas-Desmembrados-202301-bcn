68f3cab652d950643daeab294c35e3c8fac08a39
Ana Coronel <a9nna.coronel@gmail.com>
2023-02-10 08:43:18 +0100

    Add test to Character component

--- src/components/Character/Character.tsx
+++ src/components/Character/Character.tsx
@@ -1,24 +1,52 @@
+import { useContext } from "react";
+import { CharacterStructure } from "../../data/types";
+import CharactersContext from "../../store/contexts/characters/CharactersContext";
 import CharacterCard from "./CharacterStyled";
 
-const Character = (): JSX.Element => {
+interface CharacterProps {
+  character: CharacterStructure;
+}
+
+const Character = ({
+  character: {
+    image,
+    name,
+    origin: { name: origin },
+    species,
+  },
+}: CharacterProps): JSX.Element => {
+  useContext(CharactersContext);
+
   return (
-    <CharacterCard className="card">
-      <div className="card__header">
-        <span>Rick Sanchez</span>
-        <img src="img/favourite-empty.png" alt="Empty add to my characters" />
-      </div>
-      <img src="img/rick-sample.png" alt="A card with Rick Sanchez" />
-      <div className="card__resumed-info">
-        <div className="card__origin">
-          <span>Origin</span>
-          <span>C-137</span>
+    <li>
+      <CharacterCard className="card">
+        <div className="card__header">
+          <span>{name}</span>
+          <img
+            src="img/favourite-empty.png"
+            alt="Empty add to my characters"
+            width={45}
+            height={45}
+          />
         </div>
-        <div className="card__specie">
-          <span>Specie</span>
-          <span>Human</span>
+        <img
+          src={`${image}`}
+          alt={`A card with ${name}`}
+          width={300}
+          height={300}
+        />
+        <div className="card__resumed-info">
+          <div className="card__origin">
+            <span>Origin</span>
+            <span>{origin}</span>
+          </div>
+          <div className="card__specie">
+            <span>Specie</span>
+            <span>{species}</span>
+          </div>
         </div>
-      </div>
-    </CharacterCard>
+      </CharacterCard>
+    </li>
   );
 };
 
--- src/components/Character/CharacterStyled.tsx
+++ src/components/Character/CharacterStyled.tsx
@@ -1,77 +1,90 @@
 import styled from "styled-components";
 
 const CharacterCard = styled.div`
+  font-family: "Source Code Pro", sans-serif;
   display: flex;
-  flex-wrap: wrap;
   flex-direction: column;
-  position: absolute;
-  width: 300px;
-  height: 400px;
-  background-color: #ff9d9d;
   border-radius: 12px;
   overflow: hidden;
-  font-family: "Source Code Pro", sans-serif;
 
   .card {
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: column;
+    position: absolute;
+    width: 300px;
+    height: 400px;
+
     &__header {
-      display: flex;
-      justify-content: space-around;
+      display: grid;
+      justify-content: space-between;
       align-items: center;
       position: absolute;
       width: 300px;
-      height: 62px;
+      height: 65px;
+      padding: 5px;
+      border-radius: 12px 12px 0 0;
       background-color: #feeb70;
       box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
-      font-family: "Source Code Pro", sans-serif;
       font-weight: bold;
 
       & span {
         font-size: 25px;
+        grid-column: 1/2;
+      }
+
+      & img {
+        grid-column: 2/3;
       }
     }
 
     &__resumed-info {
       display: flex;
-      flex-direction: column;
-      flex: 1;
-      justify-content: space-around;
-      align-items: center;
+      flex-wrap: wrap;
+      height: 150px;
+      width: 300px;
       background-color: #feeb70;
     }
 
     &__origin {
-      display: flex;
-      gap: 50px;
+      width: 300px;
+      display: grid;
+      grid-template-columns: 150px 150px;
+      align-items: center;
+      justify-items: center;
+      font-size: 18px;
 
       & span {
         font-weight: 600;
-      }
-
-      & span:first-child {
-        font-weight: bold;
-      }
-
-      & > :nth-child(n) {
-        display: inline-block;
-        font-size: 25px;
-      }
-    }
-
-    &__specie {
-      display: flex;
-      gap: 50px;
-
-      & span {
-        font-weight: 600;
+        grid-column: 2/3;
       }
 
       & span:first-child {
         font-weight: 700;
+        grid-column: 1/2;
       }
 
       & > :nth-child(n) {
-        display: inline-block;
-        font-size: 25px;
+        text-align: center;
+      }
+    }
+
+    &__specie {
+      width: 300px;
+      display: grid;
+      grid-template-columns: 150px 150px;
+      align-items: center;
+      justify-items: center;
+      font-size: 18px;
+
+      & span {
+        font-weight: 600;
+        grid-column: 2/3;
+      }
+
+      & span:first-child {
+        font-weight: 700;
+        grid-column: 1/2;
       }
     }
   }
