d18f67c36fef592497f1a2aec45506a970a4541e
Ana Coronel <a9nna.coronel@gmail.com>
2023-02-10 11:44:01 +0100

    Add styles to Characters component

---
+++ src/components/Characters/CharactersStyled.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const CharactersList = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  gap: 20px;
+`;
+
+export default CharactersList;
