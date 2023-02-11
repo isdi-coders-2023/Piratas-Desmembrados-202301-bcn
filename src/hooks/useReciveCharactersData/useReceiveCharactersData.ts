e804bc974f7881d4682127d62a31f1f17fdcd917
lfilella97 <lfilella97@gmail.com>
2023-02-11 19:33:23 +0100

    Modify useReceiveCharactersData function to set isLoading

--- src/hooks/useReciveCharactersData/useReceiveCharactersData.ts
+++ src/hooks/useReciveCharactersData/useReceiveCharactersData.ts
@@ -1,7 +1,12 @@
 import { useContext, useCallback } from "react";
 import { RickAndMortyApiDataStructure } from "../../data/types";
 import { loadApiDataActionCreator } from "../../store/actions/characters/charactersActionCreator";
+import {
+  setLoaderOffAction,
+  setLoaderOnAction,
+} from "../../store/actions/ui/uiActionCreator";
 import CharactersContext from "../../store/contexts/characters/CharactersContext";
+import UiContext from "../../store/contexts/UiContext/UiContext";
 
 export interface UseReceiveCharactersDataStructure {
   getRickApiData: () => void;
@@ -10,12 +15,17 @@ export interface UseReceiveCharactersDataStructure {
 export const useReceiveCharactersData =
   (): UseReceiveCharactersDataStructure => {
     const { dispatch } = useContext(CharactersContext);
+    const { dispatch: uiDispatch } = useContext(UiContext);
 
     const getRickApiData = useCallback(async () => {
+      uiDispatch(setLoaderOnAction());
+
       const response = await fetch(process.env.REACT_APP_API_URL!);
       const apiData = (await response.json()) as RickAndMortyApiDataStructure;
+
+      uiDispatch(setLoaderOffAction());
       dispatch(loadApiDataActionCreator(apiData));
-    }, [dispatch]);
+    }, [dispatch, uiDispatch]);
 
     return { getRickApiData };
   };
