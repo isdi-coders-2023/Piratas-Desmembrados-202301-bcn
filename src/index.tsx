caf29620eec300eee7620a04e378b9c3cba782c4
Ana Coronel <a9nna.coronel@gmail.com>
2023-02-11 19:28:29 +0100

    Update feature/router branch

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,30 @@ import ReactDOM from "react-dom/client";
 import "@fontsource/source-code-pro/700.css";
 import "@fontsource/source-code-pro/600.css";
 import "@fontsource/source-code-pro/400.css";
-import App from "./App";
 import { GlobalStyles } from "./styles/GlobalStyles";
 import CharacterContextProvider from "./store/contexts/characters/CharacterContextProvider";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ErrorPage from "./pages/ErrorPage/ErrorPage";
+import App from "./App";
+import HomePage from "./pages/HomePage/HomePage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+      {
+        path: "errorpage",
+        element: <ErrorPage />,
+      },
+    ],
+  },
+]);
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -14,7 +35,7 @@ root.render(
   <React.StrictMode>
     <CharacterContextProvider>
       <GlobalStyles />
-      <App />
+      <RouterProvider router={router} />
     </CharacterContextProvider>
   </React.StrictMode>
 );
