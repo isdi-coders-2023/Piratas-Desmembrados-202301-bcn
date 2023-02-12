5124ce5d1185b421dd1fdcdcbe5589096200aef2
Ana Coronel <a9nna.coronel@gmail.com>
2023-02-12 15:41:27 +0100

    Add padding to Characters

--- src/components/Characters/CharactersStyled.tsx
+++ src/components/Characters/CharactersStyled.tsx
@@ -3,11 +3,12 @@ import styled from "styled-components";
 const CharactersList = styled.ul`
   position: absolute;
   z-index: -1;
-  top: 125px;
+  top: 90px;
   display: flex;
   flex-wrap: wrap;
   justify-content: space-around;
   gap: 20px;
+  padding: 40px;
 `;
 
 export default CharactersList;
