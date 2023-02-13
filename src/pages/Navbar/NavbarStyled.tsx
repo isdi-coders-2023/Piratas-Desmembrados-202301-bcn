962e2128f0e8862b72bb1e2b57a63265af8d85e1
Victor Granda <victorgranda1992@gmail.com>
2023-02-13 15:39:24 +0100

    Delete box shadow from navbar

--- src/pages/Navbar/NavbarStyled.tsx
+++ src/pages/Navbar/NavbarStyled.tsx
@@ -3,7 +3,6 @@ import styled from "styled-components";
 const NavbarStyled = styled.nav`
   width: 100%;
   height: 55px;
-  border-bottom: 2px solid #f1f1f1;
   padding: 0 20px;
   display: flex;
   justify-content: space-between;
